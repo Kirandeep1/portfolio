afa1c606edcc966f0464ed235dfaffd84c5f1cd9
Kirandeep1 <kiran.kaur1099@gmail.com>
2024-12-21 20:49:09 +0530

    hostbinding dynamic class changes

--- src/app/baner/baner.component.ts
+++ src/app/baner/baner.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, HostBinding } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 @Component({
   selector: 'app-baner',
   imports: [],
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './baner.component.css'
 })
 export class BanerComponent {
+@HostBinding('class.pc') pcMode = false;
+// dynamically adds or removes the pc class on the host element based on the value of the pcMode property.
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.HandsetPortrait, Breakpoints.WebLandscape]).subscribe({next: result => {
+      for (let breakpoint of Object.keys(result.breakpoints)) {
+        console.log("breakpoint: ", breakpoint);
+        if (result.breakpoints[breakpoint]) {
+          if (breakpoint === Breakpoints.HandsetPortrait) {
+            this.pcMode = false;
+          } else if (breakpoint === Breakpoints.WebLandscape) {
+            this.pcMode = true;
+          }
+        }
+      }
+    }});
+  }
 }
--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-personal-information',
   imports: [],
   templateUrl: './personal-information.component.html',
   styleUrl: './personal-information.component.css'
 })
-export class PersonalInformationComponent {
+export class PersonalInformationComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {}
 
 }
