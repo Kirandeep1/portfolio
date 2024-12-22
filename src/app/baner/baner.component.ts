a5db01100e7dc840a859e5ba7879302775af0993
Kirandeep1 <kiran.kaur1099@gmail.com>
2024-12-22 22:52:50 +0530

    basic baner view created

--- src/app/baner/baner.component.ts
+++ src/app/baner/baner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-baner',
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './baner.component.css'
 })
 export class BanerComponent {
-  constructor() { }
+  constructor(private element:ElementRef) { }
+  scrollToProjects() {
+    const projectsSection = document.querySelector('app-projects');
+    projectsSection?.scrollIntoView({ behavior: 'smooth' });
+  }
+  downloadResume(): void {
+    const resumeUrl = '/Kirandeep_Kaur_Resume.pdf'; // Replace with your resume's actual path
+    const link = this.element.nativeElement.ownerDocument.createElement('a');
+    link.href = resumeUrl;
+    link.download = 'Kirandeep_Kaur_Resume.pdf';
+    link.click();
+  }
 
   ngOnInit() {
   }
