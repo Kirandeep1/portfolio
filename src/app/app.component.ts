d8563aff6aef7b98f0928bf4c8be856daa5ec3f6
Kirandeep1 <kiran.kaur1099@gmail.com>
2025-01-01 01:01:38 +0530

    education and projects section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,11 @@ import { ResponsiveDirective } from './directives/responsive.directive';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { ExperienceComponent } from "./experience/experience.component";
+import { EducationComponent } from './education/education.component';
 
 @Component({
   selector: 'app-root',
-  imports: [BanerComponent, ResponsiveDirective, SkillsComponent, PersonalInformationComponent, ProjectsComponent, AboutMeComponent, NavbarComponent, ContactComponent, ExperienceComponent],
+  imports: [BanerComponent, ResponsiveDirective, SkillsComponent, PersonalInformationComponent, ProjectsComponent, AboutMeComponent, NavbarComponent, ContactComponent, ExperienceComponent, EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-
+interface Project {
+  title: string;
+  technologies: string[];
+  description: string;
+  responsibilities: string[];
+  time_worked: string;
+}
 @Component({
   selector: 'app-projects',
   imports: [],
@@ -8,4 +14,47 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 title: string = "Projects";
+projects: Project[] = [
+  {
+    title: 'Market Data on Demand Application',
+    technologies: ['Angular', 'GraphQL', 'Node.js', 'Go'],
+    description:
+      'A financial data application that provides end of day market data to clients on demand. Supports data requests for multiple securities via CSV or Excel import',
+    responsibilities: [
+      'Developed the front-end using Angular from scratch; now maintained by another team.',
+    ],
+    time_worked: "06/2021-03/2024"
+  },
+  {
+    title: 'gRPC Service Management Tool',
+    technologies: ['Retool', 'gRPC'],
+    description:
+      'A Retool-based tool for managing gRPC services, improving coordination between operations and development teams.',
+    responsibilities: [
+      'Built and maintaining the tool independently.',
+      'Integrated back-end services and collaborated with operations for requirements.',
+    ],
+    time_worked: "08/2024-Present"
+  },
+  {
+    title: 'Vendor Management System',
+    technologies: ['Angular', 'Java'],
+    description:
+      'Manages vendor and client data distribution for financial products, focusing on authentication and authorization.',
+    responsibilities: [
+      'Responsible for the enhancements and bug fixes of the app.',
+    ],
+    time_worked: "01/2021-Present"
+    },
+    {
+      title: 'Agreement App',
+      technologies: ['AngularJS 1.x', 'Java'],
+      description:
+        'Allows clients to sign agreements online for exchange data access, enabling product authorization.',
+      responsibilities: [
+        'Involved in the maintenance and bug fixes of the app.',
+      ],
+      time_worked: "10/2022-Present"
+      },
+];
 }
